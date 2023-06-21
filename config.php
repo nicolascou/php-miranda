d425679fe384584358a6c1d35467fac4d0cd4487
nicolascou <nicolascousillas1@gmail.com>
2023-06-21 12:28:52 +0200

    add query in room controller

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$DB_HOST = 'localhost';
+$DB_USER = 'root';
+$DB_NAME = 'miranda';
+$DB_PASSWORD = 'ThE900rs_?';
--- src/controllers/rooms.php
+++ src/controllers/rooms.php
@@ -1,10 +1,14 @@
 <?php
 
 include "../../vendor/autoload.php";
+require_once "../database/database.php";
 
 use eftec\bladeone\BladeOne;
+
+$db = new Database;
+$result = $db->query('SELECT * FROM rooms LIMIT 9');
 
 $views = '../views';
 $cache = '../cache';
 $blade = new BladeOne($views,$cache,BladeOne::MODE_DEBUG);
-echo $blade->run("rooms",[]);
+echo $blade->run("rooms",array('rooms'=>$result));
