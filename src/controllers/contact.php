1d8903fda2907b54e1200a79c6289ba444fd0902
nicolascou <nicolascousillas1@gmail.com>
2023-06-22 11:37:34 +0200

    add contact form

--- src/controllers/contact.php
+++ src/controllers/contact.php
@@ -1,10 +1,27 @@
 <?php
 
 include "../../vendor/autoload.php";
+require_once "../database/database.php";
 
 use eftec\bladeone\BladeOne;
 
-$views = '../views';
-$cache = '../cache';
-$blade = new BladeOne($views,$cache,BladeOne::MODE_DEBUG);
-echo $blade->run("contact",[]);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $db = new Database;
+    $name = $_POST['full_name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $date = date('Y-m-d');
+    
+    $db->query("INSERT INTO contact 
+        (date, name, email, phone, subject, comment, archived)
+        VALUES (?, ?, ?, ?, ?, ?, ?)", [$date, $name, $email, $phone, $subject, $message, 0]);
+    
+    header("Location: contact.php");
+} else {
+    $views = '../views';
+    $cache = '../cache';
+    $blade = new BladeOne($views,$cache,BladeOne::MODE_DEBUG);
+    echo $blade->run("contact",[]);
+}
